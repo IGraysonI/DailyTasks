2666d623fcc5dbe721a4ccdf7a5c2394b04ebbd2
Grayson <Agnarr.Sigurdsson@gmail.com>
2026-07-03 12:24:31 +0500

    fix: Correct asset path for empty data image in NoDataWidget

--- packages/ui/lib/src/widget/no_data_widget.dart
+++ packages/ui/lib/src/widget/no_data_widget.dart
@@ -59,8 +59,7 @@ class NoDataWidget extends StatelessWidget {
               ),
               height: MediaQuery.of(context).size.width * .5,
               width: MediaQuery.of(context).size.height * .5,
-              // FIXME: Asset not found exception
-              child: Image.asset('assets/images/empty_data.png', fit: BoxFit.contain),
+              child: Image.asset('packages/ui/assets/images/empty_data.png', fit: BoxFit.contain),
             ),
           ),
         ),
